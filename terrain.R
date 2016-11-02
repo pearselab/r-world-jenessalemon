b8f15b388b3e910f3ca1a645dcc84b452acbefab
Your Name <jimblotter@Jenessa.local>
2016-11-02 12:04:47 -0600

    Class Nov. 2

--- terrain.R
+++ terrain.R
@@ -14,6 +14,65 @@ How to do this?
                         for(i in steps){
                         matrix[...,...] <- ...     '''
 ############################################################################################################
+terrain <- matrix(data = NA, nrow = 21, ncol = 21)
+wd <- ncol
+ht <- nrow
+diamond.step <- function(matrix){
+  terrain[1, nrow] <- rnorm(1, 0, 1)
+  terrain[ncol, nrow] <- rnorm(1, 0, 1)
+  terrain[ncol, 1] <- rnorm(1, 0, 1)
+  terrain[1, 1] <- rnorm(1, 0, 1)
+}
+diamond.step(terrain)
 
+terrain <- matrix(data = NA, nrow = 9, ncol = 9)
+row.vector <- c(1, 5, 9)
+col.vector <- c(1, 5, 9)
+for (i in row.vector){
+  for (j in col.vector){
+    if (is.na(terrain[i,j])){
+    terrain[i,j] <- rnorm(1, 10, 100)
+    }else{
+      cat("Already a number there!")
+    }
+  }
+}
+print(terrain)  
+  
+
+  
+  
+  
+  
+  
+  
+  
+  
+  terrain.matrix <- function(n){
+    matrix <- matrix(data=NA, nrow = (2*n)+1, ncol = (2*n)+1)       #define matrix
+    matrix[1,1] <- rnorm(1,0,15)                                    #defining 4 corners
+    matrix[(2*n)+1,1] <- rnorm(1,0,15)
+    matrix[1,(2*n)+1] <- rnorm(1,0,15)
+    matrix[(2*n)+1,(2*n)+1] <- rnorm(1,0,15)
+    return(matrix)
+  }
+  terrain.matrix(5)                                                #checking to see if it worked
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+
+  
+  
+  
 
  
