c6410d27d48dcaa5bf550f21087c2bc4c69a3e40
Your Name <jimblotter@Jenessa.local>
2016-11-07 11:18:56 -0700

    made a matrix

--- plants.R
+++ plants.R
@@ -1,6 +1,22 @@
+repro <- c(0.3, 0.7, 0.4)
+survive <- c(0.4, 0.6, 0.3)
+comp.mat <- matrix(data = NA, nrow=3, ncol=3)
+comp.mat[1, 1] <- 2
+comp.mat[1, 2] <- 5
+comp.mat[1, 3] <- 4
+comp.mat[2, 1] <- 5
+comp.mat[2, 2] <- 1
+comp.mat[2, 3] <- 3
+comp.mat[3, 1] <- 6
+comp.mat[3, 2] <- 8
+comp.mat[3, 3] <- 7
+comp.mat #just a check
+names <- c(shockley, soredium, longilobum)
+
+
 setup.plants <- function(repro, survive, comp.mat, names = NULL){
   if(is.null(names)){
-    names <- letters[seq_along(repro)]
+    names <- letters[seq_along(repro)] #is this just assigning names a, b, c, if it doesn't already have a name?
   }
   if(length(repro) != length(survive){
     stop("Reproduction and survival parameters needed for all species!")
