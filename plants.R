55a3b8f38821ce8c27d984697b06ab3af8ef2f10
Your Name <jimblotter@Jenessa.local>
2016-11-08 14:33:01 -0700

    just keep swimming, swimming, swimming

--- plants.R
+++ plants.R
@@ -1,19 +1,31 @@
 #Parameters
-rep <- c(0.3, 0.7, 0.4)
-sur <- c(0.4, 0.6, 0.3)
-comp.matrix <- matrix(data = NA, nrow=3, ncol=3)
-comp.matrix[1, 1] <- .2
-comp.matrix[1, 2] <- .9
-comp.matrix[1, 3] <- .4
-comp.matrix[2, 1] <- .5
-comp.matrix[2, 2] <- .1
-comp.matrix[2, 3] <- .3
-comp.matrix[3, 1] <- .6
-comp.matrix[3, 2] <- .8
-comp.matrix[3, 3] <- .7
-comp.matrix #just a check
-names <- c(shockley, soredium, longilobum)
+rep <- runif(n=4, min=0, max=1) #runif because we don't want negative probabilities!
+rep #just checking
 
+sur <- survive <- runif(n=length(rep), min=0, max=1) #again we don't want negatives, and we want the same length
+sur #just checking
+
+comp.list <- runif(n=length(rep)^2, min = 0, max = 1)
+comp.list #just checking
+comp.matrix <- matrix(comps, nrow = length(rep), ncol = length(rep))
+comp.matrix
+#### I manually created a matrix before I saw Carol's cool way to randomly generate one.
+#comp.matrix <- matrix(data = NA, nrow=3, ncol=3)
+#comp.matrix[1, 1] <- .2
+#comp.matrix[1, 2] <- .9
+#comp.matrix[1, 3] <- .4
+#comp.matrix[2, 1] <- .5
+#comp.matrix[2, 2] <- .1
+#comp.matrix[2, 3] <- .3
+#comp.matrix[3, 1] <- .6
+#comp.matrix[3, 2] <- .8
+#comp.matrix[3, 3] <- .7
+#comp.matrix #just a check
+
+names <- list("shockleyi", "soredium", "longilobum") #does it matter if we use a list or a vector?
+
+char.matrix <- matrix(data=NA, nrow = length(rep), ncol = length(rep)) #initializing a matrix of appropriate size.
+char.matrix #just checking
 
 setup.plants <- function(repro, survive, comp.mat, names = NULL){
   if(is.null(names)){
@@ -28,23 +40,15 @@ setup.plants <- function(repro, survive, comp.mat, names = NULL){
   if(ncol(comp.mat) != length(repro)){
     "Competition and reproduction values needed for all species"
   }
+  if(any(repro > 1) | any(repro < 0)){
+    stop("Invalid probability")
+  }
+  survive <- setNames(survive, names)
   repro <- setNames(repro, names)
   return(list(repro=repro, survive=survive, comp.mat=comp.mat, names=names))
 }
-info <- setup.plants(rep, sur, comp.matrix) #calling to see if it works and saving it as info for later.
+info <- setup.plants(rep, sur, comp.matrix, names) #calling to see if it works and saving it as info for later.
 info
-
-char.matrix <- matrix(data = NA, nrow=3, ncol=3)
-char.matrix[1, 1] <- " "
-char.matrix[1, 2] <- " "
-char.matrix[1, 3] <- " "
-char.matrix[2, 1] <- " "
-char.matrix[2, 2] <- " "
-char.matrix[2, 3] <- " "
-char.matrix[3, 1] <- " "
-char.matrix[3, 2] <- " "
-char.matrix[3, 3] <- " "
-char.matrix #just a check
 
 plant.timestep(plants, terrain, info){
   survive <- function(cell, info){
@@ -62,7 +66,7 @@ plant.timestep(plants, terrain, info){
 }
 plant.timestep(char.matrix, terrain, info) #calling to see if it works
 
-run.plant.ecosystem <- function(){
+run.plant.ecosystem <- function(terrain){
   plants <- array("", dim=c(terrain), timesteps+1)) #why timesteps +1?
   for(i in seq_len(dim(plants)[3])){
     plants[,,i][is.na(terrain)] <- NA
