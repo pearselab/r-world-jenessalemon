cd08c3dc94ef4bb4688f26783d2126ec4bdf9af5
Your Name <jimblotter@Jenessa.local>
2016-11-08 09:40:50 -0700

    plants

--- plants.R
+++ plants.R
@@ -1,16 +1,17 @@
-repro <- c(0.3, 0.7, 0.4)
-survive <- c(0.4, 0.6, 0.3)
-comp.mat <- matrix(data = NA, nrow=3, ncol=3)
-comp.mat[1, 1] <- 2
-comp.mat[1, 2] <- 5
-comp.mat[1, 3] <- 4
-comp.mat[2, 1] <- 5
-comp.mat[2, 2] <- 1
-comp.mat[2, 3] <- 3
-comp.mat[3, 1] <- 6
-comp.mat[3, 2] <- 8
-comp.mat[3, 3] <- 7
-comp.mat #just a check
+#Parameters
+rep <- c(0.3, 0.7, 0.4)
+sur <- c(0.4, 0.6, 0.3)
+comp.matrix <- matrix(data = NA, nrow=3, ncol=3)
+comp.matrix[1, 1] <- .2
+comp.matrix[1, 2] <- .9
+comp.matrix[1, 3] <- .4
+comp.matrix[2, 1] <- .5
+comp.matrix[2, 2] <- .1
+comp.matrix[2, 3] <- .3
+comp.matrix[3, 1] <- .6
+comp.matrix[3, 2] <- .8
+comp.matrix[3, 3] <- .7
+comp.matrix #just a check
 names <- c(shockley, soredium, longilobum)
 
 
@@ -18,10 +19,43 @@ setup.plants <- function(repro, survive, comp.mat, names = NULL){
   if(is.null(names)){
     names <- letters[seq_along(repro)] #is this just assigning names a, b, c, if it doesn't already have a name?
   }
-  if(length(repro) != length(survive){
+  if(length(repro) != length(survive)){
     stop("Reproduction and survival parameters needed for all species!")
   }
-  #more tests...
+  if(nrow(comp.mat) != ncol(comp.mat)){
+    stop("Please enter a square matrix")
+  }
+  if(ncol(comp.mat) != length(repro)){
+    "Competition and reproduction values needed for all species"
+  }
   repro <- setNames(repro, names)
   return(list(repro=repro, survive=survive, comp.mat=comp.mat, names=names))
 }
+setup.plants(rep, sur, comp.matrix)
+
+survive <- function(cell, info){
+  if(is.na(cell) || cell == " "){
+    #do something
+  }
+  if(runif(1) <= info$survive[plant]){
+    #i think i need to store the plant in the cell
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
