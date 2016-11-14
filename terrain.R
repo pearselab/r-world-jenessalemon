0905c16c5e2b28a734a5e27008e0947c92e73704
Your Name <jimblotter@Jenessa.local>
2016-11-14 10:57:47 -0700

    Nov14

--- terrain.R
+++ terrain.R
@@ -1,18 +1,5 @@
 '''Diamond Step Algorithm:
-  1. Make a square matrix with odd dimensions (table?)
-  2. Pick starting heights for each colorConverter
-  3. Repeat: (do this with all sub squares until all are filled.)
-      a. Across all squares within matrix
-      b. Diamond step- found corners, found their center, made it equal to the average of the four surrounding things
-      c. Square-step- top middle is equal to the average of the right left corner and middle.
-  STOP when matrix is filled. 
-  
-How to do this?
-1. Write a square step ... square.step(matrix) Find midpoint add rmorn to add noise?
-2. Write a diamond step ... diamond.step(matrix)
-3. Write a wrapper ... terrain(matrix){
-                        for(i in steps){
-                        matrix[...,...] <- ...     '''
+
 ############################################################################################################
 setup.matrix <- function(dimen){
   if(dimen %% 2 == 0){
@@ -81,7 +68,13 @@ diamond.square.step <- function(dimensions){
 diamond.square.step(terrain)
 #Awesome. Now I need a way to navigate through the matrix
 
-make.terrian <- function(x){
-  
+make.terrian <- function(matrix){
+  n <- (ncol + 1)/2
+  diamond.step(matrix[1:n], matrix[1:n])
+  square.step(matrix[1:n], matrix[1:n])
+  return(matrix)
 }
-  
+make.terrain(setup)
+setup
+
+
