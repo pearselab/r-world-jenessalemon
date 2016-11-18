29426326da71905fa08b15f01c88b29544739a61
Your Name <jimblotter@Jenessa.local>
2016-11-18 11:16:17 -0700

    Simple Package

---
+++ Test/R/double.test.R
@@ -0,0 +1,9 @@
+#' This function doubles x
+#' @param x, the number to be doubled
+#' @author Jenessa Lemon
+#' @return a doubled number
+#' @examples double(2)
+#' @export
+double <- function(x){
+  x <- x*2
+}
